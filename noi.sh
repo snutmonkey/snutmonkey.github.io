a13e7aa23bc9e8aeef9b744be1e19e41c6e33f94
snutmonkey <62071309+snutmonkey@users.noreply.github.com>
2020-08-17 12:16:07 +0200

    Update noi.sh

--- noi.sh
+++ noi.sh
@@ -4,7 +4,7 @@
 # In my case, it is set in ~/.bash_profile and gets loaded with the -l parameter above
 
 #should change this to you
-github_username=datvance
+github_username=snutmonkey
 
 #maybe flimsy, could just hard-code it
 project_dir="$( cd "$( dirname "$0" )" && pwd )"
@@ -16,5 +16,5 @@ changes=$(git status --short ${project_dir}/docs/index.html);
 if [ -n "$changes" ]; then
 	timestamp=$(date +"%Y-%m-%d_%H-%M-%S")
 	git commit ${project_dir}/docs/index.html -m "Build ${timestamp}"
-	git push https://${NOINSTAGRAM_GITHUB_TOKEN}:x-oauth-basic@github.com/${github_username}/noinstagram.git master
+	git push https://${d82c241c6cf4eafb46336b47bb46d21bd0e530e2}:x-oauth-basic@github.com/${snutmonkey}/noinstagram.git master
 fi
